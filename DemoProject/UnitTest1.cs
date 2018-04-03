7c1e5ffe5253253fcf92ff88edc02d5e2f14d151
santhoshiksr <santhoshir9@gmail.com>
2018-04-03 16:39:26 +0530

    Added all files

---
+++ DemoProject/UnitTest1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support;
+using OpenQA.Selenium.Chrome;
+namespace DemoProject
+{
+    [TestFixture]
+    public class UnitTest1
+    {
+       IWebDriver driver;
+
+        [SetUp]
+        public void startBrowser()
+        {
+            driver = new ChromeDriver();
+        }
+
+        [Test]
+        public void test()
+        {
+            driver.Url = "https://www.google.com";
+         string title=  driver.Title;
+        }
+
+        [TearDown]
+        public void closeBrowser()
+        {
+            driver.Close();
+        }
+
+    }
+   }
+
