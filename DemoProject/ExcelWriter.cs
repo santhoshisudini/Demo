9e1e23488e1fb040bb3f3ebb11a21f6f28c648b6
santhoshiksr <santhoshir9@gmail.com>
2018-04-25 16:17:40 +0530

    Changes

---
+++ DemoProject/ExcelWriter.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace DemoProject
+{
+    public class ExcelWriter
+    {
+            /// <summary>
+            /// Constructs a new instance.
+            /// </summary>
+            public ExcelWriter()
+            {
+                // Do not delete - a parameterless constructor is required!
+            }
+
+
+            public void Driver(int row, int col, string time, string sheetName)
+            {
+
+                string sDataFile = "Ranorex_Reports.xls";
+                string sFilePath = Path.GetFullPath(sDataFile);
+
+                string sOldvalue = "Automation\\bin\\Debug\\" + sDataFile;
+                sFilePath = sFilePath.Replace(sOldvalue, "") + "PEPI_Performance\\ExecutionReport\\" + sDataFile;
+                fnOpenExcel(sFilePath, sheetName);
+                writeExcel(row, col, time);
+                fnCloseExcel();
+            }
+            Excel.Application exlApp;
+            Excel.Workbook exlWB;
+            Excel.Sheets excelSheets;
+            Excel.Worksheet exlWS;
+            //Open Excel file
+            public int fnOpenExcel(string sPath, string iSheet)
+            {
+
+                int functionReturnValue = 0;
+                try
+                {
+
+                    exlApp = new Excel.Application(); //Microsoft.Office.Interop.Excel.Application();
+                    exlApp.Visible = true;
+                    exlWB = exlApp.Workbooks.Open(sPath, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing);
+
+                    // get all sheets in workbook
+                    excelSheets = exlWB.Worksheets;
+
+                    // get some sheet
+                    //string currentSheet = "Cycle1";
+                    exlWS = (Excel.Worksheet)excelSheets.get_Item(iSheet);
+                    functionReturnValue = 0;
+                }
+                catch (Exception ex)
+                {
+                    functionReturnValue = -1;
+                    Report.Error(ex.Message);
+                }
+                return functionReturnValue;
+            }
+
+
+            // Close the excel file and release objects.
+            public int fnCloseExcel()
+            {
+                //exlWB.Close();
+
+                try
+                {
+                    exlApp.ActiveWorkbook.Save();
+                    exlApp.Quit();
+
+                    System.Runtime.InteropServices.Marshal.ReleaseComObject(exlWS);
+                    System.Runtime.InteropServices.Marshal.ReleaseComObject(exlWB);
+                    System.Runtime.InteropServices.Marshal.ReleaseComObject(exlApp);
+
+                    GC.GetTotalMemory(false);
+                    GC.Collect();
+                    GC.WaitForPendingFinalizers();
+                    GC.Collect();
+                    GC.GetTotalMemory(true);
+                }
+                catch (Exception ex)
+                {
+                    Report.Error(ex.Message);
+                }
+                return 0;
+            }
+
+            public void writeExcel(int i, int j, string time)
+            {
+                Excel.Range exlRange = null;
+                exlRange = (Excel.Range)exlWS.UsedRange;
+                ((Excel.Range)exlRange.Cells[i, j]).Formula = time;
+
+            }
+
+        }
+  }
+
+
--- DemoProject/UnitTest1.cs
+++ DemoProject/UnitTest1.cs
@@ -1,29 +1,77 @@
 ﻿using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using System.Collections.Generic;
+using System.Linq;
+
 namespace DemoProject
 {
     [TestFixture]
     public class UnitTest1
     {
+        private static readonly log4net.ILog log =
+            log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
         IWebDriver driver;
         [SetUp]
         public void Initialize()
         {
             driver = new ChromeDriver();
         }
+        
         [Test,Order(1)]
-        public void CheckpageTitle()
-        {
-            driver.Url = "https://dynamics.microsoft.com/en-us/";
-            Assert.AreEqual(driver.Title,"Microsoft Dynamics 365: Intelligent Business Applications");
-        }
-        [Test,Order(2)]
         public void CheckpageStatus()
         {
-            driver.Url = "https://dynamics.microsoft.com/en-us/heloo";
-            Assert.AreEqual(driver.Title,"Page Not Found | Microsoft Dynamics 365");
+            driver.Url = "https://dynamics.microsoft.com/en-us/";
+            Assert.AreNotEqual(driver.Title,"Page Not Found | Microsoft Dynamics 365");
         }
+        [Test,Order(2)]
+        public void VerifyH1Tag()
+        {
+            driver.Url = "https://dynamics.microsoft.com/en-us/";
+            int H1Tagcount;
+            List<IWebElement> H1Tags = driver.FindElements(By.TagName("h1")).ToList();
+            H1Tagcount = H1Tags.Count;
+            Assert.AreEqual(H1Tagcount, 1);
+        }
+        [Test]
+        public void VerfiyMetaProperties()
+        {
+            driver.Url = "https://dynamics.microsoft.com/en-us/";
+            var MetaTitle = driver.FindElement(By.XPath("//meta[@name='title']@content")).Text;
+            Assert.NotNull(MetaTitle);
+            var MetaDescription = driver.FindElement(By.XPath("//meta[@name='description']@content")).Text;
+            Assert.NotNull(MetaDescription);
+            var MetaKeywords = driver.FindElement(By.XPath("//meta[@name='description']@content")).Text;
+            Assert.NotNull(MetaKeywords);
+        }
+
+        [Test]
+        public void VerifyClcid()
+        {
+            driver.Url = "https://dynamics.microsoft.com/en-us/";
+            List<IWebElement> H1Tags = driver.FindElements(By.TagName("a")).ToList();
+            foreach(IWebElement H1Tag in H1Tags)
+            {
+                if (H1Tag.GetAttribute("href").Contains("https://go.microsoft.com/fwlink/"))
+                {
+                    string value;
+                    value = H1Tag.GetAttribute("href");
+                    Assert.IsTrue(value.Contains("clcid"),value);
+                }
+            }
+
+        }
+
+        [Test]
+        public void FindBrokenLinks()
+        {
+
+        }
+        [Test,Explicit]
+        public void Logging() {
+            driver.Url = "https://dynamics.microsoft.com/en-us/";
+            log.Info("Browser Launched");
+        } 
 
         [TearDown]
         public void EndTest()
